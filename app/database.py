b5bf4e69210b1eecea64d8d4acc3ee6b11fd07bb
Joshua Simon <joshualearn62@gmail.com>
2026-03-20 16:44:50 +0100

    Saving progress before break

--- app/database.py
+++ app/database.py
@@ -5,6 +5,6 @@ DATABASE_URL = "postgresql://postgres:Josh123@localhost/POS_DB"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+print("MAIN FILE LOADED")
+
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from app.database import engine, Base, SessionLocal
+from app import models
 
 app = FastAPI()
 
+# Create tables
+Base.metadata.create_all(bind=engine)
+
+# DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.get("/")
 def root():
-    return {"message": "POS backend is running"}
+    return {"message": "working"}
+
+@app.post("/categories/")
+def create_category(db: Session = Depends(get_db)):
+    return {"message": "category endpoint working"}
